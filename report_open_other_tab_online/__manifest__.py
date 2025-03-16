af24236b655258b2493e88e501e8e39ef5d16b28
julius3419 <julius3419@gmail.com>
2025-03-16 15:57:20 +0500

    Fix cover
    Signed-off-by: julius3419 <julius3419@gmail.com>

--- report_open_other_tab_online/__manifest__.py
+++ report_open_other_tab_online/__manifest__.py
@@ -7,7 +7,7 @@
     'description': """This module enhances the Odoo reporting experience by enabling developers to open reports in a new tab for PDF or HTML. This functionality allows developers to directly preview changes made to reports simply by refreshing the report's tab. switch between PDF and HTML formats directly through the URL, eliminating the need for additional steps and saving valuable development time.
     """,
     'author': 'Rajeel',
-    'images': ['static/description/cover.png'],
+    'images': ['static/description/cover.gif'],
     'depends': ['web'],
     'data': [
         'views/ir_actions_report.xml',

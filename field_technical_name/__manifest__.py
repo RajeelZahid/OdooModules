7eb0914ffbd067bd3314ba2a861dd3e24bcdf21f
RajeelZahid <rjlrjpt@gmail.com>
2025-07-12 14:40:49 +0500

    Field Technical Name add missing things
    Signed-off-by: RajeelZahid <rjlrjpt@gmail.com>

--- field_technical_name/__manifest__.py
+++ field_technical_name/__manifest__.py
@@ -1,11 +1,14 @@
 {
     'name': 'Field Technical Name in List/Form Views',
     'version': '18.0.0.1.0',
-    'summary': '',
     'category': 'Tools',
+    'summary': 'Display and copy field technical names next to field labels',
+    'description': """This module displays the technical field name directly next to the field label in form and list views.
+Each field comes with a dedicated copy button in form view.""",
     'author': 'Rajeel',
     'license': 'LGPL-3',
     'depends': ['web'],
+    'images': ['static/description/cover.gif'],
     'data': [
     ],
     'assets': {

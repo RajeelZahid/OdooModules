d13feb3b0c527605e2eb984c51296eda7fe43e9e
RajeelZahid <rjlrjpt@gmail.com>
2025-07-12 14:25:47 +0500

    Field Technical Name module
    Signed-off-by: RajeelZahid <rjlrjpt@gmail.com>

---
+++ field_technical_name/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Field Technical Name in List/Form Views',
+    'version': '18.0.0.1.0',
+    'summary': '',
+    'category': 'Tools',
+    'author': 'Rajeel',
+    'license': 'LGPL-3',
+    'depends': ['web'],
+    'data': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'field_technical_name/static/src/views/**/*',
+        ]
+    },
+    'installable': True,
+    'application': False,
+}

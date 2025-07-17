a113d8bba67530d6e1e83adb295ef6ad8143248d
RajeelZahid <rjlrjpt@gmail.com>
2025-07-17 22:28:30 +0500

    Search many2one records with pattern module
    Signed-off-by: RajeelZahid <rjlrjpt@gmail.com>

---
+++ many2one_pattern_search/models/base.py
@@ -0,0 +1,17 @@
+from odoo import api, fields, models
+from odoo.osv import expression
+
+
+def wildcard_wrap(string):
+    return '%' + '%'.join(string) + '%'
+
+
+class Base(models.AbstractModel):
+    _inherit = 'base'
+
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        if self._context.get('pattern_search'):
+            operator = '=ilike'
+            name = wildcard_wrap(name)
+        return super(Base, self).name_search(name=name, args=args, operator=operator, limit=limit)

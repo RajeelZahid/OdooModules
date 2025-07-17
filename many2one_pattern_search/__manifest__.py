dd82fdff71ab8644dc5209a81219fe260606e8c5
RajeelZahid <rjlrjpt@gmail.com>
2025-07-17 22:39:05 +0500

    [FIX] version in manifest of pattern search module
    Signed-off-by: RajeelZahid <rjlrjpt@gmail.com>

--- many2one_pattern_search/__manifest__.py
+++ many2one_pattern_search/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Search many2one records with pattern',
-    'version': '17.0.0.1.0',
+    'version': '18.0.0.1.0',
     'category': 'Tools',
     'summary': 'Enables smarter many2one search using partial patterns by adding `{\'pattern_search\': True}` in the field context.',
     'description': """This module improves Odoo's many2one search by allowing pattern-based searching. Instead of typing full names or prefixes, users can enter partial letter sequences (e.g., "muhra" for "Muhammad Rajeel"). Just add {'pattern_search': True} in the field context to enable this smarter, faster search.""",

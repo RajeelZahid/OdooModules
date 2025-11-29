ca5d3b6cd9af9e9697bd452fcfdb4e8fd3fc5c9b
RajeelZahid <rjlrjpt@gmail.com>
2025-11-29 17:39:37 +0500

    Add Dynamic Confirmation Message module
    Signed-off-by: RajeelZahid <rjlrjpt@gmail.com>

---
+++ dynamic_confirmation_message/static/src/views/view_button.js
@@ -0,0 +1,31 @@
+/** @odoo-module **/
+
+import { patch } from "@web/core/utils/patch";
+import { ViewButton } from "@web/views/view_button/view_button";
+
+patch(ViewButton.prototype, {
+
+    async onClick(ev) {
+        debugger;
+        if (this.props.attrs && this.props.attrs.confirm_func && this.props.attrs.confirm_func !== '') {
+            const res_id = this.props.record.resId;
+            const res_model = this.env.model.config.resModel;
+            try {
+                const confirm_message = await this.orm.call(res_model, this.props.attrs.confirm_func, [res_id]);
+                if (typeof confirm_message === "string"){
+                    if (confirm_message === '') {
+                        if (this.props.clickParams.confirm) {
+                            delete this.props.clickParams.confirm
+                        }
+                    } else {
+                        this.props.clickParams.confirm = confirm_message + `\n\nClick Ok to continue ${this.props.string}.`
+                    }
+                }
+            } catch (error) {
+                this.env.services.notification.add(`Method "${this.clickParams.confirm}" encountered an error or was not found in model "${res_model}"`, {type: 'danger'});
+                return false;
+            }
+        }
+        return super.onClick(ev);
+    }
+});

46cbd69e0f0cc62d8b5009ce2243d121cc99ff4f
RajeelZahid <rjlrjpt@gmail.com>
2025-12-06 16:15:21 +0500

    [FIX] undefined orm in dcm module
    Signed-off-by: RajeelZahid <rjlrjpt@gmail.com>

--- dynamic_confirmation_message/static/src/views/view_button.js
+++ dynamic_confirmation_message/static/src/views/view_button.js
@@ -6,12 +6,11 @@ import { ViewButton } from "@web/views/view_button/view_button";
 patch(ViewButton.prototype, {
 
     async onClick(ev) {
-        debugger;
         if (this.props.attrs && this.props.attrs.confirm_method && this.props.attrs.confirm_method !== '') {
             const res_id = this.props.record.resId;
             const res_model = this.env.model.config.resModel;
             try {
-                const confirm_message = await this.orm.call(res_model, this.props.attrs.confirm_method, [res_id]);
+                const confirm_message = await this.env.model.orm.call(res_model, this.props.attrs.confirm_method, [res_id]);
                 if (typeof confirm_message === "string"){
                     if (confirm_message === '') {
                         if (this.props.clickParams.confirm) {
